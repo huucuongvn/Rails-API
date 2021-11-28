9e49d1afe12b8e26b1358f02adf45af7af9339bc
huucuongvn <huucuongbeat95@gmail.com>
2021-11-28 16:38:23 +0700

    Logout and Authorize!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,32 @@
 class ApplicationController < ActionController::API
+  class AuthorizationError < StandardError; end
   include JsonapiErrorsHandler
 
   ErrorMapper.map_errors!(
     'ActiveRecord::RecordNotFound' =>
       'JsonapiErrorsHandler::Errors::NotFound'
   )
-  rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
 
+  rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
   rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+  rescue_from AuthorizationError, with: :authorization_error
+
+  before_action :authorize!
 
   private
+
+  def authorize!
+    raise AuthorizationError unless current_user
+  end
+
+  def access_token
+    provided_token = request.authorization&.gsub(/\ABearer\s/, '')
+    @access_token = AccessToken.find_by(token: provided_token)
+  end
+
+  def current_user
+    @current_user = access_token&.user
+  end
 
   def authentication_error
     error = {
@@ -19,5 +36,15 @@ class ApplicationController < ActionController::API
       "detail" => "You must provide valid code in order to exchange it for token."
     }
     render json: { "errors": [ error ] }, status: 401
+  end
+
+  def authorization_error
+    error = {
+      "status" => "403",
+      "source" => { "pointer" => "/headers/authorization" },
+      "title" =>  "Not authorized",
+      "detail" => "You have no right to access this resource."
+    }
+    render json: { "errors": [ error ] }, status: 403
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post 'login', to: 'access_token#create'
+  delete 'logout', to: 'access_tokens#destroy'
   resources :articles, only: [:index, :show]
 end
