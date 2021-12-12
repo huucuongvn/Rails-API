d531198da8c0dbd74326faa391959a7d508cad93
huucuongvn <huucuongbeat95@gmail.com>
2021-12-12 12:19:56 +0700

    Articles Management

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,13 @@ class ArticlesController < ApplicationController
     skip_before_action :authorize!, only: [:index, :show]
     include Paginable 
     def index
-        paginated = paginate(Article.recent)
-        render_collection(paginated)
+        articles = Article.recent.page(params[:page]).per(params[:per_page])
+        render json: articles
+        
     end
 
     def show
-        article = Article.find(params[:id])
-        render json: serializer.new(article)
+        render json: Article.find(params[:id])
     end
     
 
@@ -16,5 +16,41 @@ class ArticlesController < ApplicationController
         ArticleSerializer
     end
 
+    def create
+        article = current_user.articles.build(article_params)
+        article.save!
+            render json: article, status: :created
+    rescue
+        render json: article, adapter: :json_api,
+            serializer: ErrorSerializer,
+            status: :unprocessable_entity
+    end
+
+    def update
+        article = current_user.articles.find(params[:id])
+        article.update_attributes!(article_params)
+        render json: article, status: :ok
+    rescue ActiveRecord::RecordNotFound
+        authorization_error
+    rescue
+        render json: article, adapter: :json_api,
+            serializer: ErrorSerializer,
+            status: :unprocessable_entity
+    end
+
+    def destroy
+        article = current_user.articles.find(params[:id])
+        article.destroy
+        head :no_content
+    rescue
+        authorization_error
+    end
+
+    private
+
+    def article_params
+        params.require(:data).require(:attributes).permit(:title, :content, :slug) ||
+        ActionController::Parameters.new
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates :login, presence: true, uniqueness: true
   validates :provider, presence: true
 
+  has_many :articles, dependent: :destroy
+
   has_one :access_token, dependent: :destroy
 end
